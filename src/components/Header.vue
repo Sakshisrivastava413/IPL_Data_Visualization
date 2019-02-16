f8343b027aaef713f558ecd1accfbfde3afca23c
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 00:41:43 +0530

    added routing links in the header for navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="main-container">
-      <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #4D4D4D;">
-    <a class="navbar-brand text-white" href="/">IPL VISUALIZE</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </nav>
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #4D4D4D;">
+      <a class="navbar-brand text-white" href="/">IPL VISUALIZE</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-item nav-link" to="/">Teams</router-link>
+          <router-link class="nav-item nav-link" to="/player">Players</router-link>
+          <router-link class="nav-item nav-link" to="/venue">Venues</router-link>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -17,7 +24,7 @@ export default {
 
 <style>
   .main-container {
-    padding: 0px
+    padding: 0px;
   }
 </style>
 
