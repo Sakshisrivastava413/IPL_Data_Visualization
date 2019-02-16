9c4ba636fb601edb8547cf1666c932f4d0ce1725
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 00:59:38 +0530

    created sunburst component, added team route

---
+++ src/components/Sunburst.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div class="chart">
+    <ChartTitle :title="titleText" />
+    <chart-sunburst ref="chart" :data="chartData" :config="chartConfig"></chart-sunburst>
+  </div>
+</div>
+</template>
+
+<script>
+import { ChartSunburst } from "vue-d2b";
+import ChartTitle from "./ChartTitle.vue";
+
+export default {
+  name: "Sunburst",
+  props: [ 'titleText' ],
+  components: {
+    ChartSunburst,
+    ChartTitle
+  },
+  data() {
+    return {
+      chartData: {
+        label: "Team Details",
+        children: []
+      },
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Team from './views/Team.vue';
 
 Vue.use(Router);
 
@@ -7,5 +8,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'Team',
+      component: Team,
+    },
   ],
 });
