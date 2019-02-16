3dc78b9c083db9ba902f5de505b8dc88be1d328d
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 00:42:47 +0530

    created chart title component to show the heading of the chart

---
+++ src/components/ChartTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p class="heading">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChartTitle',
+  props: ['title']
+}
+</script>
+
+<style>
+</style>
+
+
