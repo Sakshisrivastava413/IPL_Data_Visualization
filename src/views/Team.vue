1a914bcb237f9e0e138203ecf4b61a10169e2770
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 01:00:35 +0530

    called sunburst component in team view passing chart title as prop

---
+++ src/views/Team.vue
@@ -0,0 +1,17 @@
+<template>
+  <Sunburst titleText="Teams Performance of all Seasons" />
+</template>
+
+<script>
+import Sunburst from '../components/Sunburst.vue';
+export default {
+  name: 'Team',
+  components: {
+    Sunburst
+  }
+}
+</script>
+
+<style>
+
+</style>
