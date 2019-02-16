a0cba9f65a2c790a95c0118b1be7dbb6246d37c1
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 01:28:30 +0530

    fetching data from json file and passing to sunburst chartdata

--- src/components/Sunburst.vue
+++ src/components/Sunburst.vue
@@ -2,7 +2,7 @@
 <div>
   <div class="chart">
     <ChartTitle :title="titleText" />
-    <chart-sunburst ref="chart" :data="chartData" :config="chartConfig"></chart-sunburst>
+    <chart-sunburst ref="chart" :data="chartData"></chart-sunburst>
   </div>
 </div>
 </template>
@@ -10,6 +10,7 @@
 <script>
 import { ChartSunburst } from "vue-d2b";
 import ChartTitle from "./ChartTitle.vue";
+import teams_details_json from "../../json/sunburstData.json";
 
 export default {
   name: "Sunburst",
@@ -25,8 +26,95 @@ export default {
         children: []
       },
     }
+  },
+  mounted() {
+    Object.entries(teams_details_json).forEach(([key, team]) => {
+      this.chartData.children.push({
+        label: team.name,
+        children: [
+          {
+            label: "Batting Performance",
+            children: [
+              {
+                label: "Batsmans hits",
+                children: [
+                  {
+                    label: "Fours",
+                    size: team.batting.fours
+                  },
+                  {
+                    label: "Sixes",
+                    size: team.batting.sixes
+                  }
+                ]
+              },
+              {
+                label: "Extra Runs",
+                children: [
+                  {
+                    label: "Legbyes",
+                    size: team.batting.legByesRuns
+                  },
+                  {
+                    label: "Byes",
+                    size: team.batting.byesRuns
+                  },
+                  {
+                    label: "Wides",
+                    size: team.batting.widesRuns
+                  },
+                  {
+                    label: "No Ball",
+                    size: team.batting.noBallsRuns
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            label: "Bowling Performance",
+            children: [
+              {
+                label: "Batsmans hits",
+                children: [
+                  {
+                    label: "Fours",
+                    size: team.bowling.fours
+                  },
+                  {
+                    label: "Sixes",
+                    size: team.bowling.sixes
+                  }
+                ]
+              },
+              {
+                label: "Extra Runs",
+                children: [
+                  {
+                    label: "Legbyes",
+                    size: team.bowling.legByesRuns
+                  },
+                  {
+                    label: "Byes",
+                    size: team.bowling.byesRuns
+                  },
+                  {
+                    label: "Wides",
+                    size: team.bowling.widesRuns
+                  },
+                  {
+                    label: "No Ball",
+                    size: team.bowling.noBallsRuns
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      });
+    });
   }
-}
+};
 </script>
 
 <style scoped>
