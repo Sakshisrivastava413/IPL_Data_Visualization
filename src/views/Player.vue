5b5d0bd2de2776c07b8e03843189afe51d41ba84
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 13:24:19 +0530

    imported bargraph component in player view

---
+++ src/views/Player.vue
@@ -0,0 +1,29 @@
+<template>
+  
+</template>
+
+<script>
+import BarChart from '../components/BarChart.vue';
+import batsman_json from '../../json/top10Batsmen.json';
+export default {
+  name: 'Player',
+  components: {
+    BarChart,
+  },
+  data() {
+    return {
+      TopBatsmanData: {
+        options: {},
+        data: {
+          labels: [],
+          datasets: []
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
