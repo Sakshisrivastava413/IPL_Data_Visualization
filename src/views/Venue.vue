f23a06213370197c65d0c9543ccc06e22109490c
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 17:02:22 +0530

    designed top ten venues barchart

--- src/views/Venue.vue
+++ src/views/Venue.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <BarChart
+    titleText="Top 10 Venues"
+    chartType="bar"
+    :chartData="TopVenuesChart.data"
+    :chartOptions="TopVenuesChart.options" />
 </template>
 
 <script>
@@ -25,8 +29,34 @@ export default {
     const topVenueData = [];
 
     Object.values(venue_json).forEach(venue => {
-      console.log(venue)
-    })
+      topVenueData.push({
+        vName: venue.name,
+        matchesPlayed: venue.matchesPlayed
+      })
+    });
+    this.TopVenuesChart.data = {
+      labels: topVenueData.map(t => t.vName),
+      datasets: [{
+        label: 'Total Runs',
+         data: topVenueData.map(t => t.matchesPlayed),
+         backgroundColor: '#7E94F6'
+      }
+      ]
+    };
+    this.TopVenuesChart.options = {
+      scales: {
+        xAxes: [{
+          
+        }],
+        yAxes: [{
+          ticks: {
+          beginAtZero: true,
+          maxRotation: 0,
+          minRotation: 0
+          }
+        }],
+      }
+    }
   }
 }
 </script>
