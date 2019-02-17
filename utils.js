271238cc2d1273c3bd9bca17998bdf7793de1356
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 16:11:52 +0530

    added function to get matchDetails from ball data

--- utils.js
+++ utils.js
@@ -303,6 +303,225 @@ function getMatchLoserId(match) {
   return match.Team_Name_Id == match.Match_Winner_Id ? match.Opponent_Team_Id : match.Team_Name_Id;
 }
 
+function venueDetails(matchJSON, BallDetails, teamJSON) {
+  const venueDetailsByCity = new Map();
+  // matchesWon batting and bowling first
+  matchJSON.forEach(match => {
+    if (match.Won_By == "NULL") return;
+    const allVenues = venueDetailsByCity.get(match.City_Name) || new Map();
+
+    match.Match_Loser_Id = getMatchLoserId(match);
+
+    const venue = allVenues.get(match.Venue_Name) || {
+      matchesPlayed: 0,
+      _matches: new Set(),
+      totalRuns: 0,
+      name: match.Venue_Name,
+      city: match.City_Name,
+      highestRecordedScore: -1000,
+      lowestRecordedScore: 1000,
+      highestRecodedMatch: {},
+      lowestRecordedMatch: {},
+      matchesWonBattingFirst: 0,
+      matchesWonBowlingFirst: 0,
+    };
+
+    venue._matches.add(match.Match_Id);
+    venue.matchesPlayed = venue._matches.size;
+    venue.totalRuns += BallDetails[match.Match_Id]['1'].runs + BallDetails[match.Match_Id]['2'].runs;
+
+    venue.matchesWonBattingFirst += Number(match.Toss_Decision == 'bat' && match.Toss_Winner_Id == match.Match_Winner_Id);
+    venue.matchesWonBowlingFirst += Number(match.Toss_Decision == 'field' && match.Toss_Winner_Id == match.Match_Winner_Id);
+
+    const matchDetails = i => ({
+      runs: BallDetails[match.Match_Id][i].runs,
+      overs: BallDetails[match.Match_Id][i].overs.length,
+      totalOuts: BallDetails[match.Match_Id][i].totalOuts,
+      byTeam: teamJSON[BallDetails[match.Match_Id][i].teamId]
+    });
+
+    if (BallDetails[match.Match_Id]['1'].runs > venue.highestRecordedScore) {
+      venue.highestRecordedScore = BallDetails[match.Match_Id]['1'].runs;
+      venue.highestRecodedMatch = matchDetails('1');
+    }
+
+    if (BallDetails[match.Match_Id]['2'].runs > venue.highestRecordedScore) {
+      venue.highestRecordedScore = BallDetails[match.Match_Id]['2'].runs;
+      venue.highestRecodedMatch = matchDetails('2');
+    }
+
+    if (BallDetails[match.Match_Id]['1'].runs < venue.lowestRecordedScore) {
+      venue.lowestRecordedScore = BallDetails[match.Match_Id]['1'].runs;
+      venue.lowestRecordedMatch = matchDetails('1');
+    }
+
+    if (BallDetails[match.Match_Id]['2'].runs < venue.lowestRecordedScore) {
+      venue.lowestRecordedScore = BallDetails[match.Match_Id]['2'].runs;
+      venue.lowestRecordedMatch = matchDetails('2');
+    }
+
+    allVenues.set(match.Venue_Name, venue);
+    venueDetailsByCity.set(match.City_Name, allVenues);
+
+  });
+  const venuesByCity_object = {};
+  venueDetailsByCity.forEach((venues, city) => {
+    venuesByCity_object[city] = {};
+    venues.forEach((venue, name) => {
+      venuesByCity_object[city][name] = venue;
+    });
+  });
+  return venuesByCity_object;
+}
+
+function getMatchDetails(ballByBallData) {
+  let details = {};
+  ballByBallData.forEach(ball => {
+
+    const {
+      Match_Id,
+      Innings_Id,
+      Over_Id,
+      Batsman_Scored,
+      Extra_Runs,
+      Extra_Type,
+      Dissimal_Type,
+      Team_Batting_Id,
+      Team_Bowling_Id
+    } = ball;
+
+    /**
+     *  Extra innings matches are not handled since 
+     *  I don't have enough knowledge of cricket and couldn't 
+     *  find anything relevant regarding this online, we assume the match to
+     *  get over after two innings.
+     **/
+    if (Innings_Id > 2) return;
+
+    // If null yet, initialise it.
+    if (!details[Match_Id]) {
+      details[Match_Id] = {
+        "1": {
+          balls: 0,
+          sixes: 0,
+          fours: 0,
+          legByes: 0,
+          byes: 0,
+          wides: 0,
+          noBalls: 0,
+          runs: 0,
+          extraRuns: 0,
+          overs: [],
+          caught: 0,
+          bowled: 0,
+          stumped: 0,
+          runOuts: 0,
+          totalOuts: 0,
+          teamId: Team_Batting_Id
+        },
+        "2": {
+          balls: 0,
+          sixes: 0,
+          fours: 0,
+          legByes: 0,
+          byes: 0,
+          wides: 0,
+          noBalls: 0,
+          runs: 0,
+          extraRuns: 0,
+          overs: [],
+          caught: 0,
+          bowled: 0,
+          stumped: 0,
+          runOuts: 0,
+          totalOuts: 0,
+          teamId: Team_Bowling_Id
+        }
+      }
+    }
+
+    const currentInnData = details[Match_Id][Innings_Id];
+
+    if (!currentInnData.overs.length || currentInnData.overs.length <= Number(Over_Id) - 1) {
+      if (currentInnData.overs.length) {
+        const lastOver = currentInnData.overs[currentInnData.overs.length - 1];
+        lastOver.runRate = (lastOver.runs / 6).toFixed(3);
+      }
+      currentInnData.overs.push({
+        overdId: Over_Id,
+        sixes: Number(Batsman_Scored == 6),
+        fours: Number(Batsman_Scored == 4),
+        legByes: Number(Extra_Type == "legbyes"),
+        byes: Number(Extra_Type == "byes"),
+        wides: Number(Extra_Type == "wides"),
+        noBalls: Number(Extra_Type == "noballs"),
+        extraRuns: Number(Extra_Type ? Extra_Runs : 0),
+        runs: Number(isNaN(Batsman_Scored) ? 0 : Batsman_Scored) + (Extra_Type ? Number(Extra_Runs) : 0),
+        caught: Number(Dissimal_Type == "caught" || Dissimal_Type == "caught and bowled"),
+        bowled: Number(Dissimal_Type == "bowled"),
+        stumped: Number(Dissimal_Type == "stumped"),
+        runOuts: Number(Dissimal_Type == "run out")
+      });
+    } else {
+      const overDetails = currentInnData.overs[currentInnData.overs.length - 1];
+      overDetails.sixes += Number(Batsman_Scored == 6);
+      overDetails.fours += Number(Batsman_Scored == 4);
+      overDetails.legByes += Number(Extra_Type == "legbyes");
+      overDetails.byes += Number(Extra_Type == "byes");
+      overDetails.wides += Number(Extra_Type == "wides");
+      overDetails.noBalls += Number(Extra_Type == "noballs");
+      overDetails.extraRuns += Number(Extra_Type ? Extra_Runs : 0);
+      overDetails.runs += Number(isNaN(Batsman_Scored) ? 0 : Batsman_Scored) + Number(Extra_Type ? Extra_Runs : 0);
+      overDetails.caught += Number(Dissimal_Type == "caught" || Dissimal_Type == "caught and bowled");
+      overDetails.bowled += Number(Dissimal_Type == "bowled");
+      overDetails.stumped += Number(Dissimal_Type == "stumped");
+      overDetails.runOuts += Number(Dissimal_Type == "run out");
+    }
+
+    currentInnData.balls += Number(Extra_Type != "wides");
+    currentInnData.sixes += Number(Batsman_Scored == 6);
+    currentInnData.fours += Number(Batsman_Scored == 4);
+    currentInnData.legByes += Number(Extra_Type == "legbyes");
+    currentInnData.byes += Number(Extra_Type == "byes");
+    currentInnData.wides += Number(Extra_Type == "wides");
+    currentInnData.noBalls += Number(Extra_Type == "noballs");
+    currentInnData.extraRuns += Number(Extra_Type ? Extra_Runs : 0);
+    currentInnData.runs += Number(isNaN(Batsman_Scored) ? 0 : Batsman_Scored) + Number(Extra_Type ? Extra_Runs : 0);
+    currentInnData.caught += Number(Dissimal_Type == "caught" || Dissimal_Type == "caught and bowled");
+    currentInnData.bowled += Number(Dissimal_Type == "bowled");
+    currentInnData.stumped += Number(Dissimal_Type == "stumped");
+    currentInnData.runOuts += Number(Dissimal_Type == "run out");
+    currentInnData.totalOuts += Number(
+      Dissimal_Type == "bowled" ||
+      Dissimal_Type == "caught" ||
+      Dissimal_Type == "caught and bowled" ||
+      Dissimal_Type == "bowled" ||
+      Dissimal_Type == "stumped" ||
+      Dissimal_Type == "run out"
+    );
+
+    const totalBatsmenOutYet = currentInnData.caught + currentInnData.bowled + currentInnData.runOuts + currentInnData.stumped;
+
+    /**
+     * Assumption: Data of match will always be sorted for a innning
+     */
+    const isMatchOver = (
+      Innings_Id == 2 &&
+      (totalBatsmenOutYet == 10 || Over_Id == 20 || currentInnData.runs > details[Match_Id]["1"].runs)
+    );
+
+    if (isMatchOver) {
+      // In respect of team 1 to team 2, the same will be negative of current for team 
+      details[Match_Id].Net_Run_Rate = Number(
+        (details[Match_Id][1].runs / (details[Match_Id][1].balls / 6)) -
+        (details[Match_Id][2].runs / (details[Match_Id][2].balls / 6))
+      ).toFixed(3);
+    }
+
+  });
+  return details;
+}
+
 
 module.exports = {
   convertToJSON, getMatchDetails, venueDetails, getBatsmanData, getPlayerNames, getTeamBowlingBattingData, getDataBySeasons, getTeamWinningAndLosingCount, getTeamName,
