8de2d340e3e99bf72b301e2190fde4ebfb707aa8
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-17 16:51:45 +0530

    created venue view & imported barChart component, added route for venue view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Team from './views/Team.vue';
 import Player from './views/Player.vue';
+import Venue from './views/Venue.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/player',
       name: 'Player',
       component: Player,
+    },
+    {
+      path: '/venue',
+      name: 'Venue',
+      component: Venue,
     },
   ],
 });
---
+++ src/views/Venue.vue
@@ -0,0 +1,36 @@
+<template>
+  
+</template>
+
+<script>
+import BarChart from '../components/BarChart.vue';
+import venue_json from '../../json/top10Venue.json';
+export default {
+  name: 'Venue',
+  components: {
+    BarChart,
+  },
+  data() {
+    return {
+      TopVenuesChart: {
+        options: {},
+        data: {
+          labels: [],
+          datasets: []
+        }
+      }
+    }
+  },
+  mounted() {
+    const topVenueData = [];
+
+    Object.values(venue_json).forEach(venue => {
+      console.log(venue)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
