15bbc90adc28f04e90c39ccd34ffd7e187a4f249
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-24 10:49:24 +0530

    added vue2-google-maps library, updated venueDetails.json with extra fields: country, teamDetails, and lat long of the venue.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GMAPS_API_KEY,
+    libraries: 'drawing',
+  },
+  // installComponents: true,
+})
 
 new Vue({
   router,
