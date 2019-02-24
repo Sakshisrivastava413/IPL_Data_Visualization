c0f6cacec77fec4b80e7e487bef17450d612ff7e
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-24 13:56:38 +0530

    added constant file exporting team color

---
+++ src/teamColor.constants.js
@@ -0,0 +1,32 @@
+export const TEAM_COLOR = [
+  '',
+  '#C7A9F9',
+  '#F2C5A6',
+  '#E9B1F0',
+  '#F5E6AC',
+  '#F2A9A9',
+  '#D3F0A3',
+  '#FFB8D2',
+  '#9FF1DC',
+  '#BF9898',
+  '#569BEE',
+  '#A8D0FF',
+  '#F3D2A3',
+  '#889EFF'
+];
+
+export const BUBBLE_CHART_TEAM_COLOR = [
+  '#9067D5',
+  '#D88E5B',
+  '#E9B1F0',
+  '#4A6D97',
+  '#CC62D9',
+  '#D95353',
+  '#A1D350',
+  '#CE4C7C',
+  '#46C6A5',
+  '#816363',
+  '#3D8AE7',
+  '#E19E40',
+  '#6D6D6D'
+];
