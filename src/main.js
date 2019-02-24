8a293a100c57e0e9310c5bc4ce15ccd3dd20a457
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-24 10:58:34 +0530

    added warning for not adding environment variable for gmaps

--- src/main.js
+++ src/main.js
@@ -6,13 +6,20 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false;
 
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: process.env.VUE_APP_GMAPS_API_KEY,
-    libraries: 'drawing',
-  },
-  // installComponents: true,
-})
+if (!process.env.VUE_APP_GMAPS_API_KEY) {
+  alert("Whoops! You forgot to add `VUE_APP_GMAPS_API_KEY` variable in your env file, check console for more details!");
+  console.log(`
+    %cYou have not added VUE_APP_GMAPS_API_KEY variable in your '.env' file, please add it and initialise with google maps API key
+  `, "color: red; background: lightgreen; font-size: 18px");
+}
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: process.env.VUE_APP_GMAPS_API_KEY,
+      libraries: 'drawing',
+    },
+    // installComponents: true,
+  })
+
 
 new Vue({
   router,
