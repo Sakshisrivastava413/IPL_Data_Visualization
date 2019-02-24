708d46a15369ee14654a997ff71eedce509efac5
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-02-25 01:38:36 +0530

    separated player details into BatsmanDetailCard component

---
+++ src/components/BatsmanDetailCard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="card-container">
+    <div class="card-body" v-if="player">
+      <h5 class="card-title">{{player.details.Player_Name}}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{{player.teamDetails.Team_Name}}</h6>
+      <p class="card-text">
+        Total Runs Scored: <span class="text-value">{{player.totalRuns}}</span>
+        <br />
+        Total Matches Played: <span class="text-value">{{Object.keys(player.matches).length}}</span>
+        <br />
+        Total Centuries Earned: <span class="text-value">{{player.centuries}}</span>
+        <br />
+        Batting Hand: <span class="text-value">{{player.details.Batting_Hand}}</span>
+        <br />
+        Bowling Skill: <span class="text-value">{{player.details.Bowling_Skill}}</span>
+      </p>
+    </div>
+    <div class="card-body" v-else>
+      <p class="not-selected">
+        Click any player bar to view more info.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayerDetailCard',
+  props: ['batsman'],
+  watch: {
+    batsman: {
+      handler: function(newValue) {
+        this.player = newValue;
+        console.log(this.player)
+      }
+    }
+  },
+  data() {
+    return {
+      player: this.batsman
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .card-text {
+    color: rgb(58, 180, 156)
+  }
+  .text-value {
+    color: black
+  }
+  .not-selected {
+    color: gray;
+    font-size: 20px;
+  }
+  .card-container {
+    display: inline-block;
+    position: absolute;
+    margin: 18px;
+  }
+</style>
+<style>
+  .card-container > div {
+    width: 26vw;
+    height: 30vh;
+    margin-top: 17vh;
+    border-radius: 14px;
+    box-shadow: 1px 1px 2px 2px black;
+  }
+</style>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -7,59 +7,32 @@
       :chartOptions="TopBatsmanData.options"
       :parentStyle="chartContainerStyle"
     />
-    <div class="card-container" v-if="focusedBatman">
-      <div class="card-body">
-        <h5 class="card-title">{{focusedBatman.details.Player_Name}}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">{{focusedBatman.teamDetails.Team_Name}}</h6>
-        <p class="card-text">
-          Total Runs Scored: <span class="text-value">{{focusedBatman.totalRuns}}</span>
-          <br />
-          Total Matches Played: <span class="text-value">{{Object.keys(focusedBatman.matches).length}}</span>
-          <br />
-          Total Centuries Earned: <span class="text-value">{{focusedBatman.centuries}}</span>
-        </p>
-      </div>
-    </div>
-    <div v-else class="chart-observation">
-      <div class="observation-card">
-        <p class="not-selected">
-          Click any player bar to show more.
-        </p>
-      </div>
-    </div>
+    <BatsmanDetailCard :batsman="focusedBatman" />
 
     <Chart
-      titleText="Highest Man Of the Matches Winners"
+      titleText="Highest Man Of the Match Winners"
       chartType="bar"
       :chartData="TopManOfTheMatchData.data"
       :chartOptions="TopManOfTheMatchData.options"
       :parentStyle="chartContainerStyle"
     />
-    <div class="card-container" v-if="focusedBatman">
-      <div class="card-body">
-        <h5 class="card-title">{{focusedBatman.details.Player_Name}}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">{{focusedBatman.teamDetails.Team_Name}}</h6>
-        <p class="card-text">
-          Total Runs Scored: <span class="text-value">{{focusedBatman.totalRuns}}</span>
-          <br />
-          Total Matches Played: <span class="text-value">{{Object.keys(focusedBatman.matches).length}}</span>
-          <br />
-          Total Centuries Earned: <span class="text-value">{{focusedBatman.centuries}}</span>
-        </p>
-      </div>
-    </div>
+    <BatsmanDetailCard :batsman="focusedBatman2" />
   </div>
 </template>
 
 <script>
 import Chart from '../components/Chart.vue';
-import batsman_json from '../../json/top10Batsmen.json';
+import BatsmanDetailCard from '../components/BatsmanDetailCard.vue';
+
 import manOfTheMatch_json from '../../json/topManOfTheMatch.json';
+import batsman_json from '../../json/top10Batsmen.json';
 import match_json from '../../json/matchData.json';
+
 export default {
   name: 'Player',
   components: {
-    Chart
+    Chart,
+    BatsmanDetailCard,
   },
   data() {
     return {
@@ -74,14 +47,14 @@ export default {
           scales: {
             xAxes: [{
               ticks: {
-              beginAtZero: true,
-              maxRotation: 0,
-              minRotation: 0
+                beginAtZero: true,
+                maxRotation: 0,
+                minRotation: 0
               }
             }],
           },
           onClick: (evt, item) => {
-            if (item && item[0] && item[0]._index) {
+            if (item && item[0] && item[0]._index != null) {
               this.focusedBatman = batsman_json[item[0]._index];
             }
           }
@@ -97,11 +70,16 @@ export default {
           scales: {
             yAxes: [{
               ticks: {
-              beginAtZero: true,
-              maxRotation: 0,
-              minRotation: 0
+                beginAtZero: true,
+                maxRotation: 0,
+                minRotation: 0
               }
             }],
+          },
+          onClick: (evt, item) => {
+            if (item && item[0] && item[0]._index != null) {
+              this.focusedBatman2 = batsman_json[item[0]._index];
+            }
           }
         },
         data: {
@@ -109,80 +87,58 @@ export default {
           datasets: []
         }
       },
-      focusedBatman: null
+      focusedBatman: null,
+      focusedBatman2: null
     }
   },
   mounted() {
     const topBatsmenData = [];
-    Object.values(batsman_json).forEach(team => {
-        topBatsmenData.push({
-          name: team.details.Player_Name,
-          totalRuns: team.totalRuns
-        });
-    });
 
+    Object.values(batsman_json).forEach(team => {
+      topBatsmenData.push({
+        name: team.details.Player_Name,
+        totalRuns: team.totalRuns
+      });
+    });
     this.TopBatsmanData.data = {
       labels: topBatsmenData.map(t => t.name),
       datasets: [{
         label: 'Total Runs',
-         data: topBatsmenData.map(t => t.totalRuns),
-         backgroundColor: '#76D7C4'
-      }
-      ]
+        data: topBatsmenData.map(t => t.totalRuns),
+        backgroundColor: '#76D7C4'
+      }]
     };
 
     const TopManOfTheMatchDetail = [];
     Object.values(manOfTheMatch_json).forEach(player => {
-        TopManOfTheMatchDetail.push({
-          name: player.details.Player_Name,
-          total: player.total
-        });
+      TopManOfTheMatchDetail.push({
+        name: player.details.Player_Name,
+        total: player.total
+      });
     });
 
     this.TopManOfTheMatchData.data = {
       labels: TopManOfTheMatchDetail.map(t => t.name),
       datasets: [{
         label: 'No Of Awards',
-         data: TopManOfTheMatchDetail.map(t => t.total),
-         backgroundColor: '#F36D9C'
-      }
-      ]
+        data: TopManOfTheMatchDetail.map(t => t.total),
+        backgroundColor: '#F36D9C'
+      }]
     };
   }
 }
 </script>
-
 <style scoped>
-  .card-text {
-    color: rgb(58, 180, 156)
-  }
-  .text-value {
-    color: black
-  }
-  .not-selected {
-    color: gray;
-    font-size: 20px;
-  }
-  .card-container {
-    display: inline-block;
-    position: absolute;
-    margin: 18px;
-  }
-  .chart-observation {
-    margin: 18px;
-    margin-top: 12%;
-    position: absolute;
+  .chart-container {
+    margin: 20px;
+    width: 70%;
     display: inline-block;
   }
-  .observation-card {
-    width: 30vw;
-    height: 30vh;
-  }
-
 </style>
+
 <style>
   .card-container > div {
-    width: 31vw;
+    width: 26vw;
     height: 30vh;
     margin-top: 17vh;
     border-radius: 14px;
